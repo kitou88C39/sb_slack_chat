4a1f588bea3d208b6f0e4125b875541ab82f2e27
kitou88C39 <kanfutrooper@icloud.com>
2023-05-30 11:50:19 +0900

    first commit

---
+++ src/main/java/slack_chat/SlackChatApplication.java
@@ -0,0 +1,13 @@
+package slack_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackChatApplication.class, args);
+	}
+
+}
